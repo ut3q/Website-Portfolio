30f981589504e77ee655c9ea13fcf20cf2948226
aqua <75885764+ut3q@users.noreply.github.com>
2023-04-08 22:13:49 -0500

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+			$('#fullpage').fullpage({
+				'verticalCentered': false,
+				'scrollingSpeed': 600,
+				'autoScrolling': false,
+				'css3': true,
+				'navigation': true,
+				'navigationPosition': 'right',
+			});
+		});
+
+$(function()
+{
+    new WOW().init();
+    $(".rotate").textrotator();
+})
